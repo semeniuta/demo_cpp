88b7796defaefba283621096e053d31c5443c760
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-09-14 11:45:39 +0200

    Add two return cases to demo_cpmv

--- src/demo_cpmv.cpp
+++ src/demo_cpmv.cpp
@@ -10,6 +10,22 @@
 #include "myvec.h"
 #include <iostream>
 
+MyVec<int> create_vec() {
+
+    std::cout << "Constructing a vector to be returned\n";
+
+    MyVec<int> v{};
+
+    v.print_vector();
+
+    return v;
+
+}
+
+void print_separator() {
+    std::cout << "--------\n";
+}
+
 int main() {
 
     std::cout << "Creating vec0\n";
@@ -18,18 +34,18 @@ int main() {
         vec0.push_back(i);
     }    
     vec0.print_vector();
-    std::cout << "\n";
+    print_separator();
 
     std::cout << "Creating vec1 (copy-constructed from vec0)\n";
     MyVec<int> vec1{vec0};
     vec1.print_vector();
-    std::cout << "\n";
+    print_separator();
 
     std::cout << "Creating vec2 (copy-assignment from vec0)\n";
     MyVec<int> vec2{};
     vec2 = vec0;
     vec2.print_vector();
-    std::cout << "\n";
+    print_separator();
 
     std::cout << "Creating vec3 (move-constructed from vec2)\n";
     MyVec<int> vec3{ std::move(vec2) };
@@ -37,7 +53,7 @@ int main() {
     vec2.print_vector();
     std::cout << "Now vec3:\n";
     vec3.print_vector();
-    std::cout << "\n";
+    print_separator();
 
     std::cout << "Creating vec4 (move-assigned from vec3)\n";
     MyVec<int> vec4{};
@@ -46,7 +62,19 @@ int main() {
     vec3.print_vector();
     std::cout << "Now vec4:\n";
     vec4.print_vector();
-    std::cout << "\n";
+    print_separator();
+
+    MyVec<int> vec5 = create_vec();
+    std::cout << "vec5 (returned):\n";
+    vec5.print_vector();
+    print_separator();
+
+    MyVec<int> vec6;
+    vec6.print_vector();
+    vec6 = create_vec();
+    std::cout << "vec6 (returned, but previously initialized):\n";
+    vec6.print_vector();
+    print_separator();
 
     return 0;
 }
