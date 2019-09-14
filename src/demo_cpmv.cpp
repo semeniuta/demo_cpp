52cf3b8135e287fa4077c1e750ca8665bf59d1aa
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-09-14 11:49:44 +0200

    Document the different stages in demo_cpmv

--- src/demo_cpmv.cpp
+++ src/demo_cpmv.cpp
@@ -4,6 +4,8 @@
  * 
  * vec0 --(copy)--> vec1
  * vec0 --(copy)--> vec2 --(move)--> vec3 --(move)--> vec4
+ * Return a new vector during initialization (vec5)
+ * Return a new vector after initialization (vec6)
  * 
  */
 
@@ -28,6 +30,7 @@ void print_separator() {
 
 int main() {
 
+    // Create vec0
     std::cout << "Creating vec0\n";
     MyVec<int> vec0;
     for (unsigned int i = 0; i < 12; i++) {
@@ -36,17 +39,20 @@ int main() {
     vec0.print_vector();
     print_separator();
 
+    // vec0 --(copy)--> vec1
     std::cout << "Creating vec1 (copy-constructed from vec0)\n";
     MyVec<int> vec1{vec0};
     vec1.print_vector();
     print_separator();
 
+    // vec0 --(copy)--> vec2
     std::cout << "Creating vec2 (copy-assignment from vec0)\n";
     MyVec<int> vec2{};
     vec2 = vec0;
     vec2.print_vector();
     print_separator();
 
+    // vec2 --(move)--> vec3
     std::cout << "Creating vec3 (move-constructed from vec2)\n";
     MyVec<int> vec3{ std::move(vec2) };
     std::cout << "Now vec2 (moved-from):\n"; 
@@ -55,6 +61,7 @@ int main() {
     vec3.print_vector();
     print_separator();
 
+    // vec3 --(move)--> vec4
     std::cout << "Creating vec4 (move-assigned from vec3)\n";
     MyVec<int> vec4{};
     vec4 = std::move(vec3);
@@ -64,11 +71,13 @@ int main() {
     vec4.print_vector();
     print_separator();
 
+    // Return a new vector during initialization (vec5)
     MyVec<int> vec5 = create_vec();
     std::cout << "vec5 (returned):\n";
     vec5.print_vector();
     print_separator();
 
+    // Return a new vector after initialization (vec6)
     MyVec<int> vec6;
     vec6.print_vector();
     vec6 = create_vec();
