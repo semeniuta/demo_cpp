ae014e83d684077dbde3dbaa707419aac98b8f1f
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-04-24 12:03:00 +0200

    Implement demo_search_closest

---
+++ src/demo_find_closest.cpp
@@ -0,0 +1,43 @@
+#include <iostream> // cout
+#include <vector> // vector
+#include <math.h> // fabs
+#include <algorithm> // sort
+#include "helpers.h" // print_vector
+
+// used in meshy
+// https://github.com/semeniuta/meshy/blob/master/src/shapedist.cpp
+long search_closest(const std::vector<double>& sorted_array, double val) {
+
+    auto iter_geq = std::lower_bound(sorted_array.begin(), sorted_array.end(), val);
+
+    if (iter_geq == sorted_array.begin()) {
+        return 0;
+    }
+
+    double a = *(iter_geq - 1);
+    double b = *(iter_geq);
+
+    if (fabs(val - a) < fabs(val - b)) {
+        return iter_geq - sorted_array.begin() - 1;
+    }
+
+    return iter_geq - sorted_array.begin();
+
+}
+
+int main() {
+
+    std::vector<double> numbers = {3.14, 4.89, 1.2, 9.4, 0.57, -1.9, 5.3, 4.65};
+
+    std::sort(numbers.begin(), numbers.end());
+
+    print_vector(numbers);
+
+    long idx_closest = search_closest(numbers, 5.1);
+
+    std::cout << "Index of the closest element: " << idx_closest << std::endl;
+    std::cout << "The closest element itself: " << numbers[idx_closest] << std::endl;
+
+    return 0;
+
+}
